32da98cd19b17fa35355a7ae05a94c540cd2b70f
wSedlacek <william.sedlacek@icloud.com>
2023-12-14 09:27:14 -0800

    chat gpt assisted solution

--- bin/main.ml
+++ bin/main.ml
@@ -1,78 +1,130 @@
-(*
-Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-...
-Game 100: 
-*)
+let read_lines file =
+  let contents = In_channel.with_open_bin file In_channel.input_all in
+  String.split_on_char '\n' contents
 
+;;
 
+let is_digit = function '0' .. '9' -> true | _ -> false
+let _is_symbol = function '0'.. '9' -> false | '.' -> false | _ -> true
+let is_gear = function '*' -> true | _ -> false
 
-let _get_game_id line =
-  let line = String.split_on_char ':' line in
-  let game_string = List.hd line in
-  let game_id = String.sub game_string 5 (String.length game_string - 5) in
-  int_of_string game_id
+;;
 
-
-let find_max_color_number color input_str =
-  let color_pattern = Printf.sprintf "\\([0-9]+ %s\\)" color in
-  let color_regex = Str.regexp color_pattern in
-
-  let rec aux max_num = function
-    | s :: rest ->
-      begin
-        try
-          let _ = Str.search_forward color_regex s 0 in
-          let color_part = Str.matched_string s in
-          let num = int_of_string (String.trim (Str.first_chars color_part (String.length color_part - String.length color))) in
-          let new_max = max max_num num in
-          aux new_max rest
-        with
-        | Not_found -> aux max_num rest
-      end
-    | [] -> max_num
+let extract_values rows predicate =
+  let extract_values_from_row row row_index =
+    let rec extract_value chars col_index current_value acc =
+      match chars with
+      | [] -> 
+          (match current_value with
+          | Some (value, cols) -> (value, row_index, List.rev cols) :: acc
+          | None -> acc)
+      | hd :: tl ->
+          if predicate hd then
+            let new_value, cols = 
+              match current_value with
+              | Some (value, cols) -> (value ^ (Char.escaped hd), col_index :: cols)
+              | None -> (Char.escaped hd, [col_index])
+            in
+            extract_value tl (col_index + 1) (Some (new_value, cols)) acc
+          else
+            (match current_value with
+            | Some (value, cols) -> extract_value tl (col_index + 1) None ((value, row_index, List.rev cols) :: acc)
+            | None -> extract_value tl (col_index + 1) None acc)
+    in
+    let chars = String.to_seq row |> List.of_seq in
+    extract_value chars 0 None [] in
+  
+  let rec extract_values_from_rows rows row_index acc =
+    match rows with
+    | [] -> acc
+    | hd :: tl ->
+        let values_in_row = extract_values_from_row hd row_index in
+        extract_values_from_rows tl (row_index + 1) (values_in_row @ acc)
   in
-  Str.split (Str.regexp ";") input_str
-  |> aux 0
 
-(*
-The Elf would first like to know which games would have been possible if the
-bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
-*)
+  extract_values_from_rows rows 0 []
 
+;;
 
-let rec solve_file channel sum =
-  match input_line channel with
-  | line ->
-      let line = String.trim line in
-      let max_red = find_max_color_number "red" line in
-      let max_green = find_max_color_number "green" line in
-      let max_blue = find_max_color_number "blue" line in
-        
-      (* PART 1 *)
-      (* let red_constraint = 12 in  *)
-      (* let green_constraint = 13 in *)
-      (* let blue_constraint = 14 in *)
-      (* let game_id = get_game_id line in *)
-      (* let red_ok = max_red <= red_constraint in *)
-      (* let green_ok = max_green <= green_constraint in *)
-      (* let blue_ok = max_blue <= blue_constraint in *)
-      (* let game_ok = red_ok && green_ok && blue_ok in *)
-      (* let sum = if game_ok then sum + game_id else sum in *)
+let is_adjacent (row1, cols1) (row2, cols2) =
+  let dx = abs (row1 - row2) in
+  let dy = List.fold_left min max_int (List.map (fun col1 ->
+    List.fold_left min max_int (List.map (fun col2 ->
+      abs (col1 - col2)
+    ) cols2)
+  ) cols1) in
+  dx <= 1 && dy <= 1
 
-      (* PART 2 *)
-      let product = max_red * max_green * max_blue in
-      let sum = sum + product in
+let _extract_adjacent_numbers symbols numbers =
+  let adjacent_numbers = ref [] in
 
-      solve_file channel sum
-  | exception End_of_file -> sum
+  let rec extract_adjacent_numbers_for_symbol symbol_positions =
+    match symbol_positions with
+    | [] -> ()
+    | (_, row, cols) :: rest ->
+        let nearby_numbers = List.filter (fun (_, num_row, num_cols) ->
+          is_adjacent (row, cols) (num_row, num_cols)
+        ) numbers in
+        adjacent_numbers := nearby_numbers @ !adjacent_numbers;
+        extract_adjacent_numbers_for_symbol rest
+  in
 
+  extract_adjacent_numbers_for_symbol symbols;
+  !adjacent_numbers
 
-let file_name = "part1/question.txt"
+;;
+
+let find_adjacent_numbers symbols numbers =
+  let find_adjacent_numbers_for_symbol (_, row, cols) =
+    List.filter (fun (_, num_row, num_cols) ->
+      is_adjacent (row, cols) (num_row, num_cols)
+    ) numbers
+  in
+
+  List.map (fun symbol_position ->
+    let symbol_numbers = find_adjacent_numbers_for_symbol symbol_position in
+    symbol_numbers
+  ) symbols
+
+;;
+
+let file_name = "part2/question.txt"
 let () =
-  let ic = open_in file_name in
-  let sum = solve_file ic 0 in
-  print_endline (string_of_int sum);
+  let lines = read_lines file_name in
+  let numbers = extract_values lines is_digit in
+
+  (* PART 2 *)
+  let gears = extract_values lines is_gear in
+  let adjacent_numbers = find_adjacent_numbers gears numbers in
+  let gear_ratios = List.filter (fun numbers ->
+    List.length numbers = 2
+  ) adjacent_numbers in
+
+  List.iter (fun numbers ->
+    List.iter (fun (value, _, _) -> print_string (value ^ " ")) numbers;
+    print_endline ""
+  ) gear_ratios;
+
+  let values = List.map (fun numbers ->
+    List.map (fun (value, _, _) ->  int_of_string value) numbers
+  ) gear_ratios in
+
+  let multiples = List.map (fun numbers ->
+    List.fold_left (fun acc num -> acc * num) 1 numbers
+  ) values in
+
+  let sum = List.fold_left (+) 0 multiples in
+  print_endline ("Total sum: " ^ (string_of_int sum));
+
+
+  (* PART 1 *)
+  (* let symbols = extract_values lines is_gear in *)
+  (* let adjacent_numbers = _extract_adjacent_numbers symbols numbers in *)
+  (**)
+  (* let sorted_numbers = List.sort (fun (_, row1, _) (_, row2, _) -> compare row1 row2) adjacent_numbers in *)
+  (* List.iter (fun (value, row, cols) -> *)
+  (*   Printf.printf("Value: %s, Row: %d, Cols: [%s]\n") value row (String.concat ", " (List.map string_of_int cols)) *)
+  (* ) sorted_numbers; *)
+  (**)
+  (* let sum = List.fold_left (fun acc (value, _, _) -> acc + int_of_string value) 0 adjacent_numbers in *)
+  (* print_endline ("Total sum: " ^ (string_of_int sum)) *)
